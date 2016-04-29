e139560ec11d0730e4baee5af641890b345ea9c2
Othniel Cundangan <othniel.cundangan@gmail.com>
2016-04-29 16:45:43 -0400

    Build

---
+++ Boids Flocking/Assets/Scripts/Boids/PointToCenter.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PointToCenter : MonoBehaviour {
+
+	// Update is called once per frame
+	void LateUpdate () {
+		this.transform.rotation = Quaternion.LookRotation(Vector3.zero-this.transform.position, Vector3.up);
+	}
+}
